8913a77938e67f4e57b31bd4cbf6af436c167bb7
Adam Petrone <adampetrone83@gmail.com>
2012-12-29 11:15:15 -0500

    logging script

---
+++ core/logging.lua
@@ -0,0 +1,10 @@
+module( ..., package.seeall )
+
+
+function message( message_type, message )
+	print( "[" .. message_type:upper() .. "] " .. message )
+end
+
+function verbose( msg )
+	message( "verbose", tostring(msg) )
+end
