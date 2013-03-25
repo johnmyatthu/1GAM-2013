616150cb66ce85b3609cde3ca83757779cd5c256
Adam Petrone <adampetrone83@gmail.com>
2013-03-24 23:51:57 -0400

    Added missing func_loot

---
+++ core/func_loot.lua
@@ -0,0 +1,24 @@
+require "core"
+require "core.baseentity"
+
+func_loot = class( "func_loot", AnimatedSprite )
+function func_loot:initialize()
+	AnimatedSprite:initialize(self)
+	self.collision_mask = 0
+end
+
+function func_loot:onSpawn( params )
+	self:loadSprite( "assets/sprites/items.conf" )
+	self:playAnimation( "idle" )
+
+	AnimatedSprite.onSpawn( self, params )
+end
+
+function func_loot:onDraw( params )
+	AnimatedSprite.onDraw( self, params )
+end
+
+
+function func_loot:onRemove( params )
+	self.light.intensity = 0
+end
