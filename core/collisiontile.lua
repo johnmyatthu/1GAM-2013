68fa0a9b680e2f443018a7cae816d533ee82992f
Adam Petrone <adampetrone83@gmail.com>
2013-05-03 14:14:37 -0400

    draw with collision tile

--- core/collisiontile.lua
+++ core/collisiontile.lua
@@ -5,3 +5,10 @@ CollisionTile = class( "CollisionTile", Entity )
 function CollisionTile:initialize()
 	Entity.initialize(self)
 end
+
+function CollisionTile:draw( params )
+	love.graphics.setColor(0, 255,255,128)
+	love.graphics.rectangle( "line", self.world_x-(self.frame_width/2), self.world_y-(self.frame_height/2), self.frame_width, self.frame_height )	
+	Entity.onDraw( self, params )
+	logging.verbose( "drawing" )
+end
